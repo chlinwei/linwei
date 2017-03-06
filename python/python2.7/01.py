6a64e0b41edd28f87b700dfdd3d94719b515069f
shenge <862350707@qq.com>
2017-03-06 20:35:57 +0800

    修改了7basemodule.txt

--- python/python2.7/01.py
+++ python/python2.7/01.py
@@ -1,4 +1,11 @@
 #_*_coding:utf-8 _*_
 
-import logging
+from ConfigParser import ConfigParser
+conf = ConfigParser()
+conf.read("config.ini")
+fp = open("config.ini", "w")
+conf.remove_section('host4')  # 删除[host4]
+conf.remove_option('host3', 'pass')  # 删除[host3]的pass选项
+conf.write(fp)
 
+print "hello,world"
